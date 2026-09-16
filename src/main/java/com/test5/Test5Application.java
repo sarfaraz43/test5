d810196476ce88558152b3dd4fcbbe48bf578909
MD SARFARAZ <151362022+sarfaraz43@users.noreply.github.com>
2026-09-16 12:10:50 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -16,7 +16,7 @@ public class Test5Application {
                 boolean isPresent = false;
                 char d = 'z';
                 float m =10.3f;
-		long f = 4502L;
+		        long f = 4502L;
                 booean isSub = true;
 		SpringApplication.run(Test5Application.class, args);
 	}
